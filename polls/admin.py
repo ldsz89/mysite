82d2557f1e71d19362acf053b5789ec7e540e349
ldsz89 <ldsz89@mail.missouri.edu>
2017-05-23 15:22:27 -0500

    Change look of admin page

--- polls/admin.py
+++ polls/admin.py
@@ -7,7 +7,7 @@ from .models import Choice, Question
 
 # Register your models here.
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -17,5 +17,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
