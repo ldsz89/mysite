06d430b193cb0267c4e9ae009d5f33f7aa452e3d
ldsz89 <ldsz89@mail.missouri.edu>
2017-05-23 14:22:36 -0500

    Reorder admin page

--- polls/admin.py
+++ polls/admin.py
@@ -10,4 +10,4 @@ from .models import Question
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
