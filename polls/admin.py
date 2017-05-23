54e54a6c7b15641ad3bb8ca3eb460ce1fe73d3da
ldsz89 <ldsz89@mail.missouri.edu>
2017-05-23 15:10:54 -0500

    Add choice to question in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -7,8 +7,8 @@ from .models import Choice, Question
 
 # Register your models here.
 
-class ChoiceInLine(admin.StackedInLine):
-    model = ChoiceInLine
+class ChoiceInline(admin.StackedInline):
+    model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -16,6 +16,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    inlines = [ChoiceInLine]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
